12299f06e00c55614381e3d9fd984646464d2b40
rueschan <rescalante21@hotmail.com>
2017-04-17 21:39:01 -0500

    Initial commit

---
+++ app/src/main/java/metodos/rueschan/mx/metodos/MainActivity.java
@@ -0,0 +1,13 @@
+package metodos.rueschan.mx.metodos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
