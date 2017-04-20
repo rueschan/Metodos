054bf38fa497cb050fac95eb97cba4c3fd83f9bf
rueschan <rescalante21@hotmail.com>
2017-04-20 09:52:21 -0500

    Menú y primera pantalla

--- app/src/main/java/metodos/rueschan/mx/metodos/MainActivity.java
+++ app/src/main/java/metodos/rueschan/mx/metodos/MainActivity.java
@@ -1,7 +1,10 @@
 package metodos.rueschan.mx.metodos;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void irGaussInput(View view) {
+        Intent intent = new Intent(this, GaussInput.class);
+        startActivity(intent);
     }
 }
